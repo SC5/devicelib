854da67773084cdbf6ee5c462b11dfdd526c5fe0
Joonas Kallio <joonas@sc5.fi>
2015-02-03 16:10:50 +0200

    trigger RFID event from 'r' keypress

---
+++ client/app/main/rfid.directive.js
@@ -0,0 +1,16 @@
+angular.module('devicelibApp').directive('rfidEvent', [
+  '$document', '$log', 'socket',
+  function($document, $log, socket) {
+    return {
+      restrict: 'A',
+      link: function() {
+        $document.bind('keypress', function(e) {
+          if (e.target.nodeName !== 'input' && e.which === 114) {
+            $log.debug('Triggering RFID event');
+            socket.socket.emit('development:rfid');
+          };
+        });
+      }
+    };
+  }
+]);
