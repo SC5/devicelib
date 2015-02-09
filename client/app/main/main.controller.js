54da35fa0a3685edd3b81430a5a95435cd927aab
Max Pagels <max.pagels@sc5.io>
2015-02-09 16:04:49 +0200

    added skeleton modal for new user dialog.
    added skeleton modal for new user dialog.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,48 +1,59 @@
 'use strict';
 
 angular.module('devicelibApp')
-  .controller('MainCtrl', function ($scope, $http, socket, $timeout, rfid, $location, $log, Device) {
-    
-    $scope.alerts = [];
-    $scope.user = null;
-    $scope.addAlert = function(type, msg) {
-      $scope.alerts.push({type: type, msg: msg});
-    };
+.controller('MainCtrl', function ($scope, $http, socket, $timeout, rfid, $location, $log, Device, Modal) {
 
-    $scope.closeAlert = function(index) {
-      $scope.alerts.splice(index, 1);
-    };
+  $scope.alerts = [];
+  $scope.user = null;
+  $scope.addAlert = function(type, msg) {
+    $scope.alerts.push({type: type, msg: msg});
+  };
 
-    $scope.devices = Device.query();
-    socket.syncUpdates('device', $scope.devices);
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
 
-    socket.syncUpdates('message', [], function(event, item) {
-      $log.debug('Message received', item);
+  $scope.devices = Device.query();
+  socket.syncUpdates('device', $scope.devices);
+
+  socket.syncUpdates('message', [], function(event, item) {
+    $log.debug('Message received', item);
+    if(item.title === 'Unregistered tag') {
+      $scope.create = Modal.confirm.newUser(function(user) {
+        // TODO validate result of modal and create new user.
+      });
+      $scope.create();
+    }
+    else {
       $scope.addAlert(item.type, item.title + ': ' + item.body);
       $timeout(function() {
         $scope.closeAlert(0);
       }, 5000);
-    });
-
-    socket.syncUpdates('user', [], function(event, user) {
-      if (user.active) {
-        $scope.user = {name: user.name};
-        $scope.showLoans = true;
-        rfid.user = user;
-        $location.path('/loan');
-      }
-    });
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('device');
-      socket.unsyncUpdates('message');
-      socket.unsyncUpdates('user');
-    });
-
-    $scope.clientFieldChanged = function(model) {
-      var device = Device.get({id:model._id}, function() {
-        device = angular.extend(device, model);
-        device.$update();
-      });
-    };
+    }
   });
+
+  socket.syncUpdates('user', [], function(event, user) {
+    if (user.active) {
+      $scope.user = {name: user.name};
+      $scope.showLoans = true;
+      rfid.user = user;
+      $location.path('/loan');
+    }
+  });
+
+  $scope.$on('$destroy', function () {
+    socket.unsyncUpdates('device');
+    socket.unsyncUpdates('message');
+    socket.unsyncUpdates('user');
+  });
+
+
+
+
+  $scope.clientFieldChanged = function(model) {
+    var device = Device.get({id:model._id}, function() {
+      device = angular.extend(device, model);
+      device.$update();
+    });
+  };
+});
--- client/components/modal/modal.service.js
+++ client/components/modal/modal.service.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('devicelibApp')
-  .factory('Modal', function ($rootScope, $modal) {
+.factory('Modal', function ($rootScope, $modal) {
     /**
      * Opens a modal
      * @param  {Object} scope      - an object to be merged with modal's scope
      * @param  {String} modalClass - (optional) class(es) to be applied to the modal
      * @return {Object}            - the instance $modal.open() returns
      */
-    function openModal(scope, modalClass) {
+     function openModal(scope, modalClass) {
       var modalScope = $rootScope.$new();
       scope = scope || {};
       modalClass = modalClass || 'modal-default';
@@ -29,46 +29,44 @@ angular.module('devicelibApp')
       confirm: {
 
         /**
-         * Create a function to open a delete confirmation modal (ex. ng-click='myModalFn(name, arg1, arg2...)')
-         * @param  {Function} del - callback, ran when delete is confirmed
+         * Create a function to open a newUser confirmation modal (ex. ng-click='myModalFn(name, arg1, arg2...)')
+         * @param  {Function} del - callback, ran when newUser is confirmed
          * @return {Function}     - the function to open the modal (ex. myModalFn)
          */
-        delete: function(del) {
-          del = del || angular.noop;
+         newUser: function(callback) {
+          callback = callback || angular.noop;
 
           /**
-           * Open a delete confirmation modal
+           * Open a newUser confirmation modal
            * @param  {String} name   - name or info to show on modal
-           * @param  {All}           - any additional args are passed staight to del callback
+           * @param  {All}           - any additional args are passed staight to callback callback
            */
-          return function() {
-            var args = Array.prototype.slice.call(arguments),
-                name = args.shift(),
-                deleteModal;
+           return function() {
 
-            deleteModal = openModal({
+            var newUserModal;
+            var user = {
+              email: '',
+              name: ''
+            };
+
+            newUserModal = openModal({
               modal: {
                 dismissable: true,
-                title: 'Confirm Delete',
-                html: '<p>Are you sure you want to delete <strong>' + name + '</strong> ?</p>',
-                buttons: [{
-                  classes: 'btn-danger',
-                  text: 'Delete',
-                  click: function(e) {
-                    deleteModal.close(e);
+                rfid: 'randomRFIDString',
+                title: 'Create new user',
+                button: {
+                  text: 'Save',
+                  click: function(name, email) {
+                    user.email = email;
+                    user.name = name;
+                    newUserModal.close(user);
                   }
-                }, {
-                  classes: 'btn-default',
-                  text: 'Cancel',
-                  click: function(e) {
-                    deleteModal.dismiss(e);
-                  }
-                }]
+                }
               }
             }, 'modal-danger');
 
-            deleteModal.result.then(function(event) {
-              del.apply(event, args);
+            newUserModal.result.then(function(event) {
+              callback.apply(event, [user]);
             });
           };
         }
