b67e1c7e5ea0541cfe0b47c888c25f3077fd8aed
Joonas Kallio <joonas@sc5.fi>
2015-02-10 17:24:51 +0200

    added paging to loan history page

--- client/app/admin/history/history.controller.js
+++ client/app/admin/history/history.controller.js
@@ -1,7 +1,55 @@
 'use strict';
 
 angular.module('devicelibApp')
-  .controller('HistoryCtrl', function ($scope, $http, socket, Loan) {
-    $scope.loans = Loan.query();
+  .controller('HistoryCtrl', function ($scope, $http, $log, $timeout, socket, Loan) {
+    var defaultLimitPerPage = 20;
+    $scope.loading = true;
+    $scope.sort = {field: 'end', type: 'desc'};
+
+    // Using $http instead of Loan $resource because of meta information (meta.totalItems)
+    $http({
+      url: '/api/loans',
+      params: {skip: 0, limit: defaultLimitPerPage, sortField: $scope.sort.field, sortType: $scope.sort.type },
+      method: 'GET'
+    })
+      .success(function(data) {
+        setLoading(false);
+        $scope.totalItems = data.meta.totalItems
+        $scope.loans = data.results;
+      });
+
     socket.syncUpdates('loan', $scope.loans);
+
+    $scope.currentPage = 1;
+    $scope.itemsPerPage = defaultLimitPerPage;
+
+    // TODO make sortable from ui
+    $scope.pageChanged = function() {
+      setLoading(true, true);
+      $scope.loans = Loan.query(
+        {
+          skip: $scope.itemsPerPage * ($scope.currentPage-1),
+          limit: defaultLimitPerPage,
+          sortField: $scope.field,
+          sortType: $scope.sort.type
+        }, function() {
+          setLoading(false);
+        });
+    };
+
+    /**
+     *
+     * @param status - Boolean loading status
+     * @param force - Boolean true will set loading immeadiately
+     */
+    function setLoading(status, force) {
+      if (force) {
+        $scope.loading = status;
+      } else {
+        $timeout(function() {
+          $scope.loading = status;
+        }, 200);
+      }
+    }
+
   });
--- server/api/loan/loan.controller.js
+++ server/api/loan/loan.controller.js
@@ -1,15 +1,58 @@
 'use strict';
 
+var Q = require('q');
 var _ = require('lodash');
 var Loan = require('./loan.model');
 
 // Get list of loans
 exports.index = function(req, res) {
-  Loan.find(function (err, loans) {
-    if(err) { return handleError(res, err); }
-    return res.json(200, loans);
+  var sortFields = ['username', 'deviceName', 'deviceLabel', 'start', 'end'];
+  var sort = {'end': 'desc'};
+  var limit = req.query.limit ? parseInt(req.query.limit) : 20;
+  var skip = req.query.skip ? parseInt(req.query.skip) : 0;
+  if (req.query.sortField && sortFields.indexOf(req.query.sortField) !== -1) {
+    var sortField = req.query.sortField.toLowerCase();
+    sort[sortField] = req.query.sortType.toLowerCase() === 'desc' ? 'desc' : 'asc';
+  }
+
+  countModels(Loan).then(function(totalCount) {
+    Loan.find()
+      .sort(sort)
+      .skip(skip)
+      .limit(limit)
+      .exec(function (err, loans) {
+        if(err) { return handleError(res, err); }
+        return res.json(200, addMeta(loans, {totalItems: totalCount}));
+      });
+  }, function(err) {
+    console.error(err);
   });
 };
+
+function addMeta(res, meta) {
+  return {
+    meta: meta,
+    results: res
+  }
+}
+
+function countModels(model) {
+  var deferred = Q.defer();
+  model.find().exec(function(err, items) {
+    if (err) {
+      deferred.reject(new Error(err));
+    } else {
+      deferred.resolve(items.length);
+    }
+  });
+  return deferred.promise;
+}
+
+exports.count = function(req, res) {
+  Loan.find().exec(function(err, loans) {
+    return res.json(200, {count: loans.length});
+  });
+}
 
 // Get a single loan
 exports.show = function(req, res) {
